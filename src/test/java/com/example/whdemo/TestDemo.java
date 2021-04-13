6cb338a5c04cafb112cd3dadd83fea575555311f
wuhui <wuhui921122@foxmail.com>
2021-04-13 13:09:13 +0800

    test

--- src/test/java/com/example/whdemo/TestDemo.java
+++ src/test/java/com/example/whdemo/TestDemo.java
@@ -1,9 +1,15 @@
 package com.example.whdemo;
 
+import java.util.HashMap;
+
 public class TestDemo
 {
     public static void main(String[] args)
     {
-
+        HashMap hashMap = new HashMap(3);
+        hashMap.put(1,"111");
+        hashMap.put(2,"222");
+        hashMap.put(3,"333");
+        String str = (String)hashMap.get(2);
     }
 }
