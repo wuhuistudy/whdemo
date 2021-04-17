638fb833fd62affc3663a7104a9816caf9f336df
wuhui <wuhui921122@foxmail.com>
2021-04-17 19:20:38 +0800

    JDK Proxy(JDK动态代理) 针对实现了接口的类进行代理 通过实现InvocationHandler 然后通过invoke方法

---
+++ src/test/java/com/example/whdemo/UserManager.java
@@ -0,0 +1,8 @@
+package com.example.whdemo;
+
+public interface UserManager {
+    //新增用户抽象方法
+    void addUser(String userName,String password);
+    //删除用户抽象方法
+    void delUser(String userName);
+}
